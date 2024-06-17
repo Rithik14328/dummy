40d84a173ca2a4da4a6ff5934c41c177e6954249
Anusha <guglothanusha25@gmail.com>
2024-06-17 14:57:41 +0530

    anusha

---
+++ regestrain.js
@@ -0,0 +1,14 @@
+const form=document.getElementById("form")
+const name=document.getElementById("name")
+const gmail=document.getElementById("gmail")
+const password=document.getElementById("password")
+const password2=document.getElementById("password2")
+const submint=document.getElementById("submit")
+
+
+function heloo(){
+    const p=document.getElementById("p");
+    p.innerHTML="hello you change the text";
+    p.style.color="Red"
+
+}
